be6e64d7aa6eadd733c3c4359f5edccba4e79ed1
Podexus23 <santon23@gmail.com>
2022-05-06 21:12:03 +0300

    faet: add msg about language control

--- src/main.js
+++ src/main.js
@@ -5,8 +5,20 @@ import textArea from './code/textarea';
 const renderPage = (function renderMainPage() {
   const { body } = document;
   const { mainArea } = textArea();
+
+  function addMessages() {
+    const howToChange = document.createElement('p');
+    const workingOS = document.createElement('p');
+    howToChange.innerHTML = 'To change language press Ctrl + Alt';
+    workingOS.innerHTML = 'Keyboard made for Windows OS';
+    return { howToChange, workingOS };
+  }
+  const { howToChange, workingOS } = addMessages();
   body.append(mainArea);
   body.append(board());
+
+  body.append(howToChange);
+  body.append(workingOS);
 });
 
 renderPage();
